fe0decd7e1fda62fc571c20f776953d05d2c1ef2
niksingla <100040265+niksingla@users.noreply.github.com>
2022-10-17 22:27:33 +0530

    Update settings.py

--- imgConverter/settings.py
+++ imgConverter/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_myenv = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_myenv)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
